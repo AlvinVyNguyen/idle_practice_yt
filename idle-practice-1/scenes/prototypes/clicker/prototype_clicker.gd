1ff553c8c335869e4112a7c0e7abc6a8cad7c9ae
Alvin Nguyen <alvinvynguyen@gmail.com>
2025-12-09 21:34:42 -0800

    👀 Created initial views and navigation using signals

--- idle-practice-1/scenes/prototypes/clicker/prototype_clicker.gd
+++ idle-practice-1/scenes/prototypes/clicker/prototype_clicker.gd
@@ -4,15 +4,24 @@ extends Control
 
 ## Reference to the label displaying the current amount of stardust created.
 @export var label : Label
+
+## Reference to the user interface
+@export var user_interface : UserInterface
+
+## View reference.
+@export var view : UserInterface.Views
+
 ## Current amount of stardust created.
 var stardust : int = 0
 
 ## Initialize the label at launch.
 func _ready() -> void:
 	update_label_text()
+	visible = false
+	user_interface.navigation_requested.connect(_on_navigation_request)
 	
 ## Create 1 stardust.
-func create_stardust():
+func create_stardust() -> void:
 	stardust += 1
 	update_label_text()
 
@@ -23,3 +32,11 @@ func update_label_text() -> void:
 ## Triggered when the create stardust button is pressed.
 func _on_button_pressed() -> void:
 	create_stardust()
+
+## Watch for navigation requests and react accordingly.
+func _on_navigation_request(requested_view : UserInterface.Views) -> void:
+	if requested_view == view:
+		visible = true
+		return
+	
+	visible = false
