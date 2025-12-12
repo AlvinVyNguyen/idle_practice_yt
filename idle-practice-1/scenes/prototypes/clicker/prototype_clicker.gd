be41d4ad34af520085c2747e5c09ea0505002100
Alvin Nguyen <alvinvynguyen@gmail.com>
2025-12-11 21:10:52 -0800

    Resource handler and another refactor

--- idle-practice-1/scenes/prototypes/clicker/prototype_clicker.gd
+++ idle-practice-1/scenes/prototypes/clicker/prototype_clicker.gd
@@ -2,9 +2,6 @@ class_name PrototypeClicker
 extends Control
 ## A clicker prototype creating stardust
 
-## Reference to the label displaying the current amount of stardust created.
-@export var label : Label
-
 ## Reference to the user interface
 @export var user_interface : UserInterface
 
@@ -13,21 +10,12 @@ extends Control
 
 ## Initialize the label at launch.
 func _ready() -> void:
-	update_label_text()
 	visible = false
 	user_interface.navigation_requested.connect(_on_navigation_request)
 	
-## Temporary function to update the label.
-func _process(_delta: float) -> void:
-	update_label_text()
-	
 ## Create 1 stardust.
 func create_stardust() -> void:
-	Game.ref.data.stardust += 1
-
-#Update the text of the label to reflect a change in stardust amount.
-func update_label_text() -> void:
-	label.text = "Stardust : %s" %Game.ref.data.stardust
+	HandlerStardust.ref.create_stardust(1)
 	
 ## Triggered when the create stardust button is pressed.
 func _on_button_pressed() -> void:
